14226e9947d83f6dc02a9d2eb7556a597a10d546
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-05 11:43:54 +0100

    solution 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import Card from "./components/Card";
 
 export default function App() {
   const fruits = [
-{ id: 1, name: "🍌 Banana", color: "yellow" },
-{ id: 1, name: "🍎 Apple", color: "green" },
-{ id: 2, name: "🍍 Pineapple", color: "yellow" },
-{ id: 3, name: "🍓 Strawberry", color: "red" },
-{ id: 4, name: "🍊 Orange", color: "orange" },
+    { id: 1, name: "🍌 Banana", color: "yellow" },
+    { id: 2, name: "🍎 Apple", color: "green" },
+    { id: 3, name: "🍍 Pineapple", color: "yellow" },
+    { id: 4, name: "🍓 Strawberry", color: "red" },
+    { id: 5, name: "🍊 Orange", color: "orange" },
   ];
 
   return (
     <div className="app">
       {fruits.map((fruit) => (                       //implicit Return
-        <card key={fruit.id} name={fruit.name} color={fruit.color}  />
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
       )
 
       )}
